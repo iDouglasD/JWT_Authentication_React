c64dae18354e12c8911dbc5700e36d6284b22745
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-02-04 06:55:42 -0300

    Release

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -1,50 +1,15 @@
 import React, { createContext, useState, useEffect } from "react";
 
-import api from "../api";
-import history from "../history";
+import useAuth from "./hooks/useAuth";
 
 const Context = createContext();
 
 function AuthProvider({ children }) {
-    const [authenticated, setAuthenticated] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-
-        if (token) {
-            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-            setAuthenticated(true);
-        }
-        setLoading(false);
-    }, []);
-
-    async function handleLogin() {
-        const {
-            data: { token },
-        } = await api.post("/authenticate");
-
-        localStorage.setItem("token", JSON.stringify(token));
-        api.defaults.headers.Authorization = `Bearer ${token}`;
-        history.push("/users");
-        setAuthenticated(true);
-    }
-
-    function handleLogout() {
-        setAuthenticated(false);
-        localStorage.removeItem("token");
-        api.defaults.headers.Authorization = undefined;
-        history.push("/login");
-    }
-
-    if (loading) {
-        return <h1>Loading...</h1>;
-    }
-
+    const { authenticated, loading, handleLogin, handleLogout } = useAuth();
     return (
         <div>
             <Context.Provider
-                value={{ authenticated, handleLogin, handleLogout }}
+                value={{ loading, authenticated, handleLogin, handleLogout }}
             >
                 {children}
             </Context.Provider>
